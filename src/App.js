3dbedbc158f9bdcff54bbfde5328b0761e1f1a94
Никита <nikprogd884@gmail.com>
2026-03-05 21:28:34 +0500

    fix supabase import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://mzziirgdhvbubiunykuj.supabase.co";
 const SUPABASE_KEY = "sb_publishable_CjKU3ZHBcAjUbD0YjWodYQ_esxjkufl";
